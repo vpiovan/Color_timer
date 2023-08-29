532d443133458ce917f81ed8f529c399bff3ae6b
vpiovan <143424415+vpiovan@users.noreply.github.com>
2023-08-29 11:43:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// ********************************************************************************************* VARIABLE(S)
+
+const colors = ["blue", "yellow", "red", "white"];
+const background = document.getElementById("background");
+const pauseButton = document.getElementById("pauseButton");
+const resetButton = document.getElementById("resetButton");
+const timer = document.getElementById("timer");
+
+let currentIndex = 0;
+let previousIndex = -1;
+let timerInterval;
+let timerValue = 5.0;
+
+// *********************************************************************************** RANDOM COLOR FUNCTION
+
+function getRandomColorIndex() {
+  let newIndex;
+  do {
+    newIndex = Math.floor(Math.random() * colors.length);
+  } while (newIndex === previousIndex);
+  previousIndex = newIndex;
+  return newIndex;
+}
+
+function changeBackgroundColor() {
+  currentIndex = getRandomColorIndex();
+  background.className = colors[currentIndex];
+}
+
+// *********************************************************************************** UPDATE TIMER FUNCTION
+
+function updateTimer() {
+  timerValue = Math.max(0, timerValue - 0.01);
+  timer.textContent = timerValue.toFixed(2) + "s";
+
+  if (timerValue <= 0.01) {
+    changeBackgroundColor();
+    timerValue = 5.0;
+  }
+}
+
+// ************************************************************************************ PAUSE TIMER FUNCTION
+
+function togglePause() {
+  if (timerInterval) {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    pauseButton.textContent = "Play";
+  } else {
+    timerInterval = setInterval(updateTimer, 10);
+    pauseButton.textContent = "Pause";
+  }
+}
+
+// ************************************************************************************ RESET TIMER FUNCTION
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+  timerValue = 5.0;
+  timer.textContent = timerValue.toFixed(2) + "s";
+  changeBackgroundColor();
+  pauseButton.textContent = "Play";
+}
+
+// ****************************************************************************************** EVENT LISTENER
+
+background.addEventListener("click", changeBackgroundColor);
+pauseButton.addEventListener("click", togglePause);
+resetButton.addEventListener("click", resetTimer);
+
+changeBackgroundColor();
+togglePause();
