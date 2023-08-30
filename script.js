33e353a409970d8b7480b96772b67d124dbcfb07
vpiovan <143424415+vpiovan@users.noreply.github.com>
2023-08-30 10:24:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function updateTimer() {
   /*timerValue = Math.max(0, timerValue - 1);*/
   timerValue = timerValue - 1;
   /*timer.textContent = timerValue.toFixed(2) + "s";*/
-  if (timerValue = 1.00) {
+  if (timerValue === 1.00) {
     background.className = "black";
   }
   if (timerValue <= 0.00) {
